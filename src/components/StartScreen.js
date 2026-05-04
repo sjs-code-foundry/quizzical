2c7d60f2b279c9a80f8c0a3bc64e5289f953ca35
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-05-04 18:14:25 +0100

    feat: Add Start Screen
    User will see this when initializing the app.

---
+++ src/components/StartScreen.js
@@ -0,0 +1,9 @@
+export default function StartScreen() {
+  return (
+    <section className="start-screen">
+      <h1>Quizzical</h1>
+      <p>Some description if needed</p>
+      <button>Start Quiz</button>
+    </section>
+  );
+}
